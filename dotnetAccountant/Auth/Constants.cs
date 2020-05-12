69d857d9a1344cee58a3b5c01fbe10cee5501524
Alex Chuang <1392alex1392@gmail.com>
2020-05-12 18:49:48 +0100

    Add: contentTypes

--- dotnetAccountant/Auth/Constants.cs
+++ dotnetAccountant/Auth/Constants.cs
@@ -2,6 +2,24 @@ using System;
 
 namespace dotnetAccountant
 {
+	public static class ContentTypes
+	{
+		public static class Application
+		{
+			private const string application = "application";
+			public const string json = application + "/json";
+			public const string javascript = application + "/javascript";
+			public const string xml = application + "/xml";
+		}
+		public static class Text
+		{
+			private const string text = "text";
+			public const string plain = text + "/plain";
+			public const string xml = text + "/xml";
+			public const string html = text + "/html";
+		}
+	}
+
 	public static class Urls{
 		public const string BaseUrl = "https://graph.microsoft.com/v1.0/";
 	}
