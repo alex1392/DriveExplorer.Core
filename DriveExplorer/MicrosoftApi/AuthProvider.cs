a87c5f634e33f6c569fb0b21f64b74f1a3cddcfc
Alex Chuang <1392alex1392@gmail.com>
2020-05-18 01:15:36 +0100

    Modify: Formatting

--- DriveExplorer/MicrosoftApi/AuthProvider.cs
+++ DriveExplorer/MicrosoftApi/AuthProvider.cs
@@ -10,21 +10,15 @@ using System.Threading;
 using System.Linq;
 using System.Security;
 
-namespace DriveExplorer.MicrosoftApi
-{
-	public class AuthProvider : IAuthenticationProvider
-	{
+namespace DriveExplorer.MicrosoftApi {
+	public class AuthProvider : IAuthenticationProvider {
 		private static AuthProvider instance;
 		public static AuthProvider Instance => instance;
 		// lazy singleton with arguments
-		public static void Initialize(IConfigurationRoot appConfig)
-		{
-			if (instance == null)
-			{
+		public static void Initialize(IConfigurationRoot appConfig) {
+			if (instance == null) {
 				instance = new AuthProvider(appConfig);
-			}
-			else
-			{
+			} else {
 				Console.WriteLine($"{typeof(AuthProvider)} has already been initialized.");
 			}
 		}
@@ -34,14 +28,12 @@ namespace DriveExplorer.MicrosoftApi
 		private string[] scopes;
 		private IAccount userAccount;
 
-		public string[] Scopes
-		{
+		public string[] Scopes {
 			get => scopes;
 			set => scopes = value;
 		}
 
-		private AuthProvider(IConfigurationRoot appConfig)
-		{
+		private AuthProvider(IConfigurationRoot appConfig) {
 			this.appConfig = appConfig;
 			scopes = appConfig[nameof(scopes)].Split(';');
 			string appId = appConfig[nameof(appId)];
@@ -61,37 +53,30 @@ namespace DriveExplorer.MicrosoftApi
 			bool GetSilently = true,
 			bool GetWithUsernamePassword = true,
 			bool GetInteractively = true,
-			bool GetWithDeviceCode = true)
-		{
+			bool GetWithDeviceCode = true) {
 			string result = null;
 			// check if user already signed in
-			if (GetSilently && userAccount != null)
-			{
+			if (GetSilently && userAccount != null) {
 				result = await GetAccessTokenSilently();
 			}
-			if (GetWithUsernamePassword && result == null)
-			{
+			if (GetWithUsernamePassword && result == null) {
 				result = await GetAccessTokenWithUsernamePassword();
 			}
-			if (GetInteractively && result == null)
-			{
+			if (GetInteractively && result == null) {
 				result = await GetAccessTokenInteractively();
 			}
-			if (GetWithDeviceCode && result == null)
-			{
+			if (GetWithDeviceCode && result == null) {
 				result = await GetAccessTokenWithDeviceCode();
 			}
 
 			return result;
 		}
 
-		public async Task<string> GetAccessTokenSilently()
-		{
+		public async Task<string> GetAccessTokenSilently() {
 			// If there is an account, call AcquireTokenSilent
 			// By doing this, MSAL will refresh the token automatically if
 			// it is expired. Otherwise it returns the cached token.
-			using (var cts = new CancellationTokenSource(Timeouts.Silent))
-			{
+			using (var cts = new CancellationTokenSource(Timeouts.Silent)) {
 				var result = await msalClient
 					.AcquireTokenSilent(scopes, userAccount)
 					.ExecuteAsync(cts.Token);
@@ -101,15 +86,12 @@ namespace DriveExplorer.MicrosoftApi
 		}
 
 		// Not applicable for personal account
-		public async Task<string> GetAccessTokenWithUsernamePassword()
-		{
-			using (var cts = new CancellationTokenSource(Timeouts.Silent))
-			{
+		public async Task<string> GetAccessTokenWithUsernamePassword() {
+			using (var cts = new CancellationTokenSource(Timeouts.Silent)) {
 				string Username = appConfig[nameof(Username)];
 				string Password = appConfig[nameof(Password)];
 				var secureString = new SecureString();
-				foreach (var c in Password)
-				{
+				foreach (var c in Password) {
 					secureString.AppendChar(c);
 				}
 				var result = await msalClient
@@ -120,13 +102,10 @@ namespace DriveExplorer.MicrosoftApi
 			}
 		}
 
-		public async Task<string> GetAccessTokenWithDeviceCode()
-		{
-			using (var cts = new CancellationTokenSource(Timeouts.Interactive))
-			{
+		public async Task<string> GetAccessTokenWithDeviceCode() {
+			using (var cts = new CancellationTokenSource(Timeouts.Interactive)) {
 				// Invoke device code flow so user can sign-in with a browser
-				var result = await msalClient.AcquireTokenWithDeviceCode(scopes, deviceCodeCallback =>
-				{
+				var result = await msalClient.AcquireTokenWithDeviceCode(scopes, deviceCodeCallback => {
 					// display instructions to let user follow the device code flow
 					Console.WriteLine(deviceCodeCallback.Message);
 					// display instructions in testing output as well
@@ -138,10 +117,8 @@ namespace DriveExplorer.MicrosoftApi
 			}
 		}
 
-		public async Task<string> GetAccessTokenInteractively()
-		{
-			using (var cts = new CancellationTokenSource(Timeouts.Interactive))
-			{
+		public async Task<string> GetAccessTokenInteractively() {
+			using (var cts = new CancellationTokenSource(Timeouts.Interactive)) {
 				var result = await msalClient
 					.AcquireTokenInteractive(scopes)
 					.ExecuteAsync(cts.Token);
@@ -150,14 +127,12 @@ namespace DriveExplorer.MicrosoftApi
 			}
 		}
 
-		///
-			/// <summary>
-			/// Implementation of <see cref="IAuthenticationProvider"/>. This method is called everytime when user make a request.
-			/// </summary>
-			/// <param name="request"></param>
-			/// <returns></returns>
-		public async Task AuthenticateRequestAsync(HttpRequestMessage request)
-		{
+		/// <summary>
+		/// Implementation of <see cref="IAuthenticationProvider"/>. This method is called everytime when user make a request.
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns></returns>
+		public async Task AuthenticateRequestAsync(HttpRequestMessage request) {
 			// attach authentication to the header of http request
 			request.Headers.Authorization = new AuthenticationHeaderValue("bearer", await GetAccessToken());
 		}
