6ef9642d32a5f491fda61099865c960786e3fdb7
Alex Chuang <1392alex1392@gmail.com>
2020-05-21 12:10:16 +0100

    Add: googleApiTests

--- DriveExplorer.Tests/GoogleApi/googleApiTests.cs
+++ DriveExplorer.Tests/GoogleApi/googleApiTests.cs
@@ -1,57 +1,134 @@
+using System.Collections.Generic;
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
 using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
 using Xunit;
+using System.ComponentModel;
 using System.Threading;
+using Google;
 
-namespace DriveExplorer.GoogleApi
-{
-	public class GoogleApiTests
-	{
+namespace DriveExplorer.GoogleApi {
+	public class GoogleApiTests {
+		private const string ClientSecretsPath = "GoogleApi/client_secret.json";
+		private string[] scopes;
+		private UserCredential credential;
+		private DriveService service;
 
-		public GoogleApiTests()
-		{
-
+		public GoogleApiTests() {
+			scopes = new[] { DriveService.Scope.Drive };
+			credential = UserCredentialProvider.FromFileAsync(
+				ClientSecretsPath,
+				scopes).Result;
+			service = new DriveService(
+				new BaseClientService.Initializer {
+					HttpClientInitializer = credential,
+					ApplicationName = nameof(DriveExplorer),
+				});
 		}
-		
 
 		[Fact]
-		public async Task OAuthTestAsync()
-		{
+		public void OAuthTest() {
+			Assert.NotNull(credential);
+		}
+
+		[Fact]
+		public async Task GetAboutTestAsync() {
 			//Given
-			UserCredential credential;
-			using (var stream = new FileStream("GoogleApi/client_secret.json", FileMode.Open, FileAccess.Read))
-			{
-				credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-					GoogleClientSecrets.Load(stream).Secrets,
-					new [] { DriveService.Scope.Drive },
-					"user",
-					CancellationToken.None);
-			}
-			var service = new DriveService(new BaseClientService.Initializer
-			{
-				HttpClientInitializer = credential,
-				ApplicationName = "googleApi",
-			});
 			//When
-			var drives = (await service.Drives.List().ExecuteAsync()).Drives;
-			var files = (await service.Files.List().ExecuteAsync()).Files;
-			Console.WriteLine("=====Drives=====");
-			foreach (var drive in drives)
-			{
+			var getRequest = service.About.Get();
+			getRequest.Fields = "*";
+			var about = await getRequest.ExecuteAsync();
+			foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(about)) {
+				string name = descriptor.Name;
+				object value = descriptor.GetValue(about);
+				Console.WriteLine($"{name}: {value}");
+			}
+			//Then
+			Assert.NotNull(about);
+		}
+
+		[Fact]
+		public async Task GetAllDrivesTestAsync() {
+			//Given
+			//When
+			var drives = new List<Drive>();
+			var request = service.Drives.List();
+			do {
+				var list = await request.ExecuteAsync();
+				drives.AddRange(list.Drives);
+				request.PageToken = list.NextPageToken;
+			} while (!string.IsNullOrEmpty(request.PageToken));
+			foreach (var drive in drives) {
 				Console.WriteLine(drive.Id + ":" + drive.Name);
 			}
-			Console.WriteLine("=====Files=====");
-			foreach (var file in files)
-			{
+			//Then
+			Assert.NotNull(drives);
+		}
+
+		[Fact]
+		public async Task GetFilesTestAsync() {
+			//Given
+
+			//When
+			var files = (await service.Files.List().ExecuteAsync()).Files;
+			foreach (var file in files) {
 				Console.WriteLine(file.Id + ":" + file.Name);
 			}
-
 			//Then
 			Assert.NotNull(files);
+		}
+
+		[Fact(Skip = "Time comsuming")]
+		public async Task GetAllFilesTestAsync() {
+			//Given
+
+			//When
+			var files = new List<File>();
+			var request = service.Files.List();
+			do {
+				var list = await request.ExecuteAsync();
+				files.AddRange(list.Files);
+				request.PageToken = list.NextPageToken;
+			} while (!string.IsNullOrEmpty(request.PageToken));
+			foreach (var file in files) {
+				Console.WriteLine(file.Id + ":" + file.Name);
+			}
+			//Then
+			Assert.NotNull(files);
+		}
+
+		[Fact]
+		public async Task GetRootFolderTestAsync() {
+			//Given
+			//When
+			var root = await service.Files.Get("root").ExecuteAsync();
+			Console.WriteLine(root.Id);
+			//Then
+			Assert.NotNull(root);
+		}
+
+		[Fact]
+		public async Task GetChildrenInRootTestAsync() {
+			//Given
+			var root = await service.Files.Get("root").ExecuteAsync();
+			//When
+			var request = service.Files.List();
+			request.Q = $"parents in '{root.Id}' and trashed = false";
+			var files = new List<File>();
+			do {
+				var fileList = await request.ExecuteAsync();
+				files.AddRange(fileList.Files);
+				request.PageToken = fileList.NextPageToken;
+			} while (!string.IsNullOrEmpty(request.PageToken));
+			foreach (var file in files)
+			{
+				Console.WriteLine(file.Id + ": " + file.Name);
+			}
+			//Then
+			
 		}
 	}
 }
